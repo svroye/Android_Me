502452fdaf9a17de7cf6ea4266960996c09e2250
Stevenvr <vanroyesteven@gmail.com>
2018-03-15 22:08:02 +0100

    Solve TODOs Exercise 2

--- app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -26,13 +26,17 @@ import android.widget.ImageView;
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
 
+import java.util.List;
+
 public class BodyPartFragment extends Fragment {
 
-    // TODO (1) Create a setter method and class variable to set and store of a list of image resources
+    // DONE (1) Create a setter method and class variable to set and store of a list of image resources
 
-    // TODO (2) Create another setter method and variable to track and set the index of the list item to display
+    // DONE (2) Create another setter method and variable to track and set the index of the list item to display
         // ex. index = 0 is the first image id in the given list , index 1 is the second, and so on
 
+    private List<Integer> mImageIds;
+    private int mListIndex;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the fragment
      */
@@ -51,14 +55,21 @@ public class BodyPartFragment extends Fragment {
         // Get a reference to the ImageView in the fragment layout
         ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
 
-        // Set the image to the first in our list of head images
-        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
-
-        // TODO (3) If a list of image ids exists, set the image resource to the correct item in that list
+        // DONE (3) If a list of image ids exists, set the image resource to the correct item in that list
         // Otherwise, create a Log statement that indicates that the list was not found
+        if (mImageIds != null){
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        }
 
         // Return the rootView
         return rootView;
     }
 
+    public void setImageIds(List<Integer> mImageIds) {
+        this.mImageIds = mImageIds;
+    }
+
+    public void setListIndex(int mListIndex) {
+        this.mListIndex = mListIndex;
+    }
 }
