51a239328cb817349c14d56285ec1a888f7e5ce1
Stevenvr <vanroyesteven@gmail.com>
2018-03-17 13:34:25 +0100

    Move MainActivity to UI package

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,9 +1,10 @@
-package com.example.android.android_me;
+package com.example.android.android_me.ui;
 
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.android_me.R;
 import com.example.android.android_me.ui.MasterListFragment;
 
 public class MainActivity extends AppCompatActivity {
