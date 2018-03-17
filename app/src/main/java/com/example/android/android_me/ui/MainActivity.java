66a9e7395203a92783f604a6ead4655100d1ee48
Stevenvr <vanroyesteven@gmail.com>
2018-03-17 13:36:07 +0100

    Modify main activity layout file name

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.android.android_me.ui;
 
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.android.android_me.R;
-import com.example.android.android_me.ui.MasterListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
     }
 }
